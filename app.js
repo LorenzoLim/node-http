d36646e461ae65d73e644616e640429ee5cb630b
LorenzoLim <renzo-lim@hotmail.com>
2017-11-22 12:10:22 +1000

    Routes Added

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-
-let students = ['Scott', 'Simon'];
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const api = require('./routes/api');
+let students = require('./students');
 
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded());
 
-app.get('/api/students', function (req, res) {
-  res.send(students);
-});
+app.use('/api', api);
 
 app.get('/students', function (req, res) {
   res.render('students', {students: students});
 });
 
 app.post('/students', function (req, res) {
-  let student = req.body.name;
+  let student = req.body.student_name;
   students.push(student);
   res.send(student);
+  res.redirect('/students');
 });
 
 app.listen(port)
